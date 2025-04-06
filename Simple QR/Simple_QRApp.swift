c8c34ea0860bebb876e7bed04f36060d9fda611a
Stephane Paquet <176050+spaquet@users.noreply.github.com>
2025-04-06 15:40:54 -0700

    Adding support to Sentry

--- Simple QR/Simple_QRApp.swift
+++ Simple QR/Simple_QRApp.swift
@@ -7,6 +7,7 @@
 
 import AVFoundation
 import CloudKit
+import Sentry
 import SwiftData
 import SwiftUI
 
@@ -38,6 +39,9 @@ struct Simple_QRApp: App {
             // Create container
             container = try ModelContainer(for: schema, configurations: [modelConfiguration])
             
+            // Call Sentry Initializer
+            initializeSentry()
+            
             // Initialize data manager
             QRDataManager.initializeShared(modelContext: container.mainContext)
             
@@ -46,6 +50,31 @@ struct Simple_QRApp: App {
         } catch {
             fatalError("Failed to create SwiftData container: \(error.localizedDescription)")
         }
+    }
+    
+    /// Function used to initialize Sentry
+    private func initializeSentry() {
+        SentrySDK.start { options in
+            options.dsn = "https://f9b747796283bb73df960fcdcf463fa9@o4509108609875968.ingest.us.sentry.io/4509108618985472"
+            options.debug = true // Enabling debug when first installing is always helpful
+            
+            // Adds IP for users.
+            // For more information, visit: https://docs.sentry.io/platforms/apple/data-management/data-collected/
+            options.sendDefaultPii = true
+            
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for tracing.
+            // We recommend adjusting this value in production.
+            options.tracesSampleRate = 1.0
+        }
+        
+        // Manually call startProfiler and stopProfiler
+        // to profile the code in between
+        SentrySDK.startProfiler()
+        // this code will be profiled
+        //
+        // Calls to stopProfiler are optional - if you don't stop the profiler, it will keep profiling
+        // your application until the process exits or stopProfiler is called.
+        SentrySDK.stopProfiler()
     }
     
     // Initialize default data if needed (first launch)
